d171c52f7558afc01d966cf6f5b9516385b50793
fahadrasheed304 <fahadrasheed304@gmail.com>
2026-06-02 15:54:51 +0500

    Add auth route group layout

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="relative min-h-screen w-full bg-background">
+      {children}
+    </div>
+  )
+}
